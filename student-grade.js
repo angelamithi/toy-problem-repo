1e6617139b6d1c2ddbf793c83db6f5e066a53b09
+ <+>
2023-09-21 17:23:18 +0300

    completed student-grade assignment

---
+++ student-grade.js
@@ -0,0 +1,31 @@
+const studentGrade=function(studentMarks){
+let marks=prompt("Enter student marks-Between 0 and 100");
+studentMarks=parseInt(marks);
+
+if (studentMarks > 79){
+    alert("Grade is 'A'");
+} 
+else if(studentMarks >= 60 && studentMarks <=79)
+{
+    alert("Grade is 'B'");
+} 
+else if (studentMarks>= 59 && studentMarks < 49)
+{
+    alert("Grade is 'C'");
+}
+else if (studentMarks >= 40 && studentMarks <=49)
+ {
+   alert("Grade is 'D'");
+
+}
+else if (studentMarks < 40) 
+{
+ alert("Grade is 'E' ");
+}
+else 
+{
+    alert("Kindly enter Marks that are between 0 and 100" );
+}
+}
+
+studentGrade();
